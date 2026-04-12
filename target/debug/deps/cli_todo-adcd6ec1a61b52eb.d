f729d71270e4c38af173cafa7432a10f8a89d40e
bhirahatees-periyasamy <spvaran123@gmail.com>
2026-04-12 05:33:44 +0530

    removed bin folders

--- target/debug/deps/cli_todo-adcd6ec1a61b52eb.d
+++
@@ -1,5 +0,0 @@
-/home/bhirahateesvaran/Projects/cli-todo/target/debug/deps/cli_todo-adcd6ec1a61b52eb.d: src/main.rs
-
-/home/bhirahateesvaran/Projects/cli-todo/target/debug/deps/libcli_todo-adcd6ec1a61b52eb.rmeta: src/main.rs
-
-src/main.rs:
