906bfd9d542909661e0d3266abe3724a3765cb3c
bhirahatees-periyasamy <spvaran123@gmail.com>
2026-04-12 05:32:54 +0530

    intital commit

---
+++ target/debug/deps/cli_todo-a8d8b1445375dd7a.d
@@ -0,0 +1,5 @@
+/home/bhirahateesvaran/Projects/cli-todo/target/debug/deps/cli_todo-a8d8b1445375dd7a.d: src/main.rs
+
+/home/bhirahateesvaran/Projects/cli-todo/target/debug/deps/libcli_todo-a8d8b1445375dd7a.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/cli_todo-adcd6ec1a61b52eb.d
@@ -0,0 +1,5 @@
+/home/bhirahateesvaran/Projects/cli-todo/target/debug/deps/cli_todo-adcd6ec1a61b52eb.d: src/main.rs
+
+/home/bhirahateesvaran/Projects/cli-todo/target/debug/deps/libcli_todo-adcd6ec1a61b52eb.rmeta: src/main.rs
+
+src/main.rs:
